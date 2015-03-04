193677a439b44f4ff116cec0d89d38ae7bcf3323
Angy1 <angelica.nm@hotmail.com>
2015-03-03 23:41:51 -0500

    Implemented tests in GroupHelper 3.1

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,32 +1,36 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
-public class GroupCreationTests extends TestBase{
-	
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
-	app.getNavigationHelper().openMainPage();
-    app.getNavigationHelper().gotoGroupsPage();
-    app.getGroupHelper().initGroupCreation();
-    GroupData group = new GroupData();
-    group.name = "group name 1";
-    group.header = "header 1";
-    group.footer = "footer 1";
-	app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupsPage();
-  }
-  
-  @Test
-  public void testEmptyGroupCreation() throws Exception {
-	app.getNavigationHelper().openMainPage();
-    app.getNavigationHelper().gotoGroupsPage();
-    app.getGroupHelper().initGroupCreation();
-    //new GroupData("", "", "").fillGroupForm(this);
-    app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupsPage();
-  }
-}
+public class GroupCreationTests extends TestBase {
 
+	
+	@Test(dataProvider = "randomValidGroupGenerator")
+	public void testGroupCreationWithValidData(GroupData group) throws Exception {
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
+
+		// save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+
+		// actions
+		app.getGroupHelper().initGroupCreation();
+		app.getGroupHelper().fillGroupForm(group);
+		app.getGroupHelper().submitGroupCreation();
+		app.getGroupHelper().returnToGroupsPage();
+
+		// save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+
+		// compare states
+		oldList.add(group);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
+	}
+
+}
